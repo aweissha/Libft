0d834b4441f1f37d8013049be4853d809a05bdcd
aweissha <aweissha@outlook.de>
2023-10-07 12:52:17 +0200

    ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,35 +6,47 @@
 /*   By: aweissha <aweissha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 18:07:34 by aweissha          #+#    #+#             */
-/*   Updated: 2023/10/06 18:34:56 by aweissha         ###   ########.fr       */
+/*   Updated: 2023/10/07 12:51:24 by aweissha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char			*dest;
-	const char		*source;
+	unsigned char			*dest;
+	const unsigned char		*source;
 	size_t			i;
 
-	dest = (char *)dst;
-	source = (const char *)src;
+	dest = (unsigned char *)dst;
+	source = (const unsigned char *)src;
 	i = 0;
-	while (i < len)
+	if (dest < source)
 	{
-		dest[i] = source[i]
-		i++;
+		while (i < len)
+		{
+			dest[i] = source[i];
+			i++;			
+		}
+	}
+	else
+	{
+		i = len;
+		while (i > 0)
+		{
+			i--;
+			dest[i] = source[i];			
+		}		
 	}
 	return (dst);
 }
 
-int main(void)
-{
-    char src[] = "helloo";
-    char dst[] = "blabla";
-    // char *src = "blabla";
-    ft_memcpy(dst, src, 6);
-    write(1, dst, 6);
-    return (0);
-}
+// int main(void)
+// {
+//     char src[] = "helloo";
+//     char dst[] = "blabla";
+//     // char *src = "blabla";
+//     ft_memcpy(dst, src, 6);
+//     write(1, dst, 6);
+//     return (0);
+// }
