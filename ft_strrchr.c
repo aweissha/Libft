b00475f8c94adacfe67f3d1913c6e69235336567
Alexander Weisshaar <aweissha@1-C-32.42heilbronn.de>
2023-10-12 19:14:01 +0200

    updated ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: aweissha <aweissha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 18:06:18 by aweissha          #+#    #+#             */
-/*   Updated: 2023/10/08 18:32:40 by aweissha         ###   ########.fr       */
+/*   Updated: 2023/10/12 19:13:33 by aweissha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,16 @@ char	*ft_strrchr(const char *s, int c)
 	size_t	last;
 
 	last = ft_strlen(s);
-	while (last >= 0)
+	if (last == 0)
+		return (NULL);
+	while (last > 0)
 	{
 		if (s[last] == c)
 			return ((char *)&s[last]);
 		last--;
 	}
+	if (s[last] == c)
+		return ((char *)&s[last]);
 	return (NULL);
 }
 
