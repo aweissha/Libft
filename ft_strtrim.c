70ffbdae275c39ad87765d0966b81c809652ba61
Alexander Weisshaar <aweissha@2-C-6.42heilbronn.de>
2023-10-15 12:59:54 +0200

    project finished

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: aweissha <aweissha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 11:08:53 by aweissha          #+#    #+#             */
-/*   Updated: 2023/10/11 17:45:25 by aweissha         ###   ########.fr       */
+/*   Updated: 2023/10/15 12:56:08 by aweissha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,27 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	char	*trimmed;
 	size_t	start;
 	size_t	end;
-	char	*trimmed;
 
 	start = 0;
 	end = ft_strlen(s1);
-	while (ft_strchr(set, s1[start]) != 0 && s1[start] != '\0')
+	while (ft_strchr(set, s1[start]) && s1[start])
 		start++;
-	while (ft_strchr(set, s1[end - 1]) != 0 && end > start)
+	while ((end > start) && ft_strchr(set, s1[end - 1]))
 		end--;
 	trimmed = malloc(end - start + 1);
 	if (trimmed == NULL)
 		return (NULL);
-	ft_strlcpy(trimmed, (s1 + start), (end - start + 1));
+	ft_strlcpy(trimmed, &s1[start], end - start + 1);
 	return (trimmed);
 }
 
 // #include <stdio.h>
 // int main(void)
 // {
-// 	char s1[] = "xxzdfhdxxzy  ";
-// 	char set[] = "xyz 	";
+// 	char s1[] = "";
+// 	char set[] = "";
 // 	printf("%s\n", ft_strtrim(s1, set));
 // }
